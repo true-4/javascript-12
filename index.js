76400174c591dde21dc3fc27999a28ad8e946c28
kuga <esixterb58@gmail.com>
2025-06-16 16:45:09 +0300

    feat: task-4

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // 2
+// Создай объект animal с методом speak, который выводит в консоль "Я животное". Создай объект dog, чтобы он наследовал animal. Вызови dog.speak() — метод должен сработать.
+
 const animal = {
   speak () {
     console.log('Я животное')
@@ -10,6 +12,10 @@ dog.__proto__ = animal
 dog.speak()
 
 // 3
+// Создай функцию Person, которая принимает name.
+// Добавь в Person.prototype метод sayHi, который выводит "Привет, меня зовут <name>".
+// Создай экземпляр и вызови sayHi.
+
 function Person () {}
 
 Person.prototype.seyHi = function (name) {
@@ -18,3 +24,17 @@ Person.prototype.seyHi = function (name) {
 
 const user = new Person()
 user.seyHi('Vlad')
+
+// 4
+// Создай объект animal с canWalk = true.
+// Создай объект rabbit, унаследованный от animal, и задай rabbit.canWalk = false.
+// Проверь, что rabbit.canWalk — false, а animal.canWalk — true.
+
+const animal2 = {
+  canWalk: true,
+}
+
+const rabbit = Object.create(animal2)
+rabbit.canWalk = false
+console.log(animal2.canWalk)
+console.log(rabbit.canWalk)
